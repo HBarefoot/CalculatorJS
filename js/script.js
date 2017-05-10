3e25330cf2fc698f56ca12d351020a0a7ed1e19d
HBarefoot <henrybarefoot1987@gmail.com>
2017-05-10 01:04:38 -0400

    modification on logic

--- js/script.js
+++ js/script.js
@@ -1,53 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event){
   var listLink = document.querySelectorAll(".btn")
   var outPut = document.querySelector("#screen")
-  var dataToScreen = []
-  var num = []
-  var num2 = []
-  var symbol
+  var dataToScreen = ""
+  var result
 
   addEvent(listLink)
 
 
   /**
-  *  This function itera over an object adding an event andler "click",
+  *  This function itera over an object adding an event Handler "click",
   */
   function addEvent(obj){
     for (var i = 0; i < obj.length; i ++){
       obj[i].addEventListener("click", function(){
-        dataToScreen.push(this.innerText)
-        if (this.innerText === "C"){
-          appendToScreen(0)
-          dataToScreen = []
-        }
-
-        console.log("testing " + this.innerText)
-        debug(this.innerText)
-
-        if (this.innerText === "+" || this.innerText === "-"
-        || this.innerText === "/" || this.innerText === "*" ||
-        this.innerText === "%"){
-          symbol = this.innerText
-        }
-        if (!parseInt(dataToScreen[0]) || !parseInt(dataToScreen[0])){
-          appendToScreen(0)
-          dataToScreen = []
-          console.log("No a number")
-        } else {
-          appendToScreen(dataToScreen.join(""))
-          if (this.innerText === "="){
-            for (var i = 0; i < dataToScreen.length; i ++){
-              if (isNaN(dataToScreen[i])){
-                num = dataToScreen.splice(0, i)
-                for (var l = 0; l < dataToScreen.length; l ++){
-                  if (isNaN(dataToScreen[l])){
-                    num2 = dataToScreen.splice(1, (l - 1))
-                  }
-                }
-                arithmetic(num, num2)
-              }
-            }
+        if (this.innerText !== "="){
+          if (this.innerText !== "C"){
+            appendToScreen(dataToScreen += this.innerText)
+            console.log(dataToScreen);
+          } else {
+            dataToScreen = ""
+            appendToScreen("0")
           }
+        } else {
+          appendToScreen(eval(dataToScreen))
         }
       })
     }
@@ -62,68 +37,5 @@ document.addEventListener("DOMContentLoaded", function(event){
   //append data to screen
   function appendToScreen(obj){
     outPut.innerText = obj
-  }
-
-  //arithmetic function
-  function arithmetic(num, num2){
-    switch (symbol) {
-      case "+":
-      var n = 0
-      var n1 = 0
-      for (var key in num){
-        n += num[key]
-      }
-      for (var key in num2){
-        n1 += num2[key]
-      }
-      appendToScreen(parseFloat(n) + parseFloat(n1))
-      break;
-      case "-":
-      var n = 0
-      var n1 = 0
-      for (var key in num){
-        n += num[key]
-      }
-      for (var key in num2){
-        n1 += num2[key]
-      }
-      appendToScreen(parseFloat(n) - parseFloat(n1))
-      break;
-      case "*":
-      var n = 0
-      var n1 = 0
-      for (var key in num){
-        n += num[key]
-      }
-      for (var key in num2){
-        n1 += num2[key]
-      }
-      appendToScreen(parseFloat(n) * parseFloat(n1))
-      break;
-      case "%":
-      var n = 0
-      var n1 = 0
-      for (var key in num){
-        n += num[key]
-      }
-      for (var key in num2){
-        n1 += num2[key]
-      }
-      appendToScreen(parseFloat(n) % parseFloat(n1))
-      break;
-      case "/":
-      var n = 0
-      var n1 = 0
-      for (var key in num){
-        n += num[key]
-      }
-      for (var key in num2){
-        n1 += num2[key]
-      }
-      appendToScreen(parseInt(n) / parseInt(n1))
-      break;
-      default:
-      console.log("Check code")
-    }
   }
 });
